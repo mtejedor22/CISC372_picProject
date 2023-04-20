cbebb1fa298d5179bff956f93a758cd2d54edfa0
Mar Tejedor Ninou <mtejedor@ubuntu-2004.eecis.udel.edu>
2023-04-19 20:48:47 -0400

    done!

---
+++ imagePthread.h
@@ -0,0 +1,33 @@
+#ifndef ___IMAGE
+#define ___IMAGE
+#include <stdint.h>
+
+#define Index(x,y,width,bit,bpp) y*width*bpp+bpp*x+bit
+
+typedef struct{
+    uint8_t* data;
+    int width;
+    int height;
+    int bpp;
+} Image;
+
+typedef struct{
+        Image *srcImage;
+        Image *destImage;
+        int kernelType; 
+        int startRow;
+        int endRow;
+} ConvImage;
+
+enum KernelTypes{EDGE=0,SHARPEN=1,BLUR=2,GAUSE_BLUR=3,EMBOSS=4,IDENTITY=5};
+
+typedef double Matrix[3][3];
+
+uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm);
+ConvImage **ArraySetUp(int numThread, Image *srcImage, Image *destImage, int kernelType);
+void *convolute(void *arg);
+void handle_threads(ConvImage **array, int totalNumThreads);
+int Usage();
+enum KernelTypes GetKernelType(char* type);
+
+#endif
